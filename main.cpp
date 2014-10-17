d10eb5da6d92532b62d38b2b7497218e9e1b25a6
Bruna Pearson <b.g.maciel-pearson@durham.ac.uk>
2014-10-17 14:21:54 +0100

    openning the robot connection

--- main.cpp
+++ main.cpp
@@ -1,9 +1,37 @@
+#include "Aria.h"
 #include <iostream>
 
-using namespace std;
 
-int main()
+int main(void)
 {
-    cout << "Hello world!" << endl;
-    return 0;
+//the serial connection(robot)
+ArSerialConnection serConn;
+
+//tcp connection (sim)
+ArTcpConnection tcpConn;
+
+//robot
+ArRobot robot;
+
+
+Aria::init();
+
+
+
+tcpConn.setPort("localhost",8101);
+//tcpConn.setPort("10.0.126.12",8101);
+//see if we can get to the simulator(true is sucess)
+if(tcpConn.openSimple())
+{
+    printf("Connecting to simulator through tcp. \n");
+    robot.setDeviceConnection(&tcpConn);
+}
+else{
+    serConn.setPort(); //default "/dev/ttyS0" or "COM1" serial port
+    printf("Could not connect to simulator, connecting to robot through serial. \n");
+    robot.setDeviceConnection(&serConn);
+}
+
+Aria::exit(0);
+return 0;
 }
