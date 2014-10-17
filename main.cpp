3a9c7a1be3f77fa78bb729574d654f8c92916b39
Bruna Pearson <b.g.maciel-pearson@durham.ac.uk>
2014-10-17 15:51:30 +0100

    changing the robot connection from simulated env to Pioneer 3AT

--- main.cpp
+++ main.cpp
@@ -1,37 +1,77 @@
 #include "Aria.h"
 #include <iostream>
+#include <stdio.h>
 
-
-int main(void)
+/* Main method */
+int main(int argc, char** argv)
 {
-//the serial connection(robot)
-ArSerialConnection serConn;
 
-//tcp connection (sim)
-ArTcpConnection tcpConn;
-
-//robot
-ArRobot robot;
+  // Initialize some global data
+  Aria::init();
 
 
-Aria::init();
+  // If you want ArLog to print "Verbose" level messages uncomment this:
+  //ArLog::init(ArLog::StdOut, ArLog::Verbose);
 
+  // This object parses program options from the command line
+  ArArgumentParser parser(&argc, argv);
 
+  // Load some default values for command line arguments from /etc/Aria.args
+  // (Linux) or the ARIAARGS environment variable.
+  parser.loadDefaultArguments();
 
-tcpConn.setPort("localhost",8101);
-//tcpConn.setPort("10.0.126.12",8101);
-//see if we can get to the simulator(true is sucess)
-if(tcpConn.openSimple())
-{
-    printf("Connecting to simulator through tcp. \n");
-    robot.setDeviceConnection(&tcpConn);
-}
-else{
-    serConn.setPort(); //default "/dev/ttyS0" or "COM1" serial port
-    printf("Could not connect to simulator, connecting to robot through serial. \n");
-    robot.setDeviceConnection(&serConn);
-}
+  // Central object that is an interface to the robot and its integrated
+  // devices, and which manages control of the robot by the rest of the program.
+  ArRobot robot;
 
-Aria::exit(0);
-return 0;
+  // Object that connects to the robot or simulator using program options
+  ArRobotConnector robotConnector(&parser, &robot);
+
+  // If the robot has an Analog Gyro, this object will activate it, and
+  // if the robot does not automatically use the gyro to correct heading,
+  // this object reads data from it and corrects the pose in ArRobot
+  ArAnalogGyro gyro(&robot);
+
+  // Connect to the robot, get some initial data from it such as type and name,
+  // and then load parameter files for this robot.
+  if (!robotConnector.connectRobot())
+  {
+    // Error connecting:
+    // if the user gave the -help argumentp, then just print out what happened,
+    // and continue so options can be displayed later.
+    if (!parser.checkHelpAndWarnUnparsed())
+    {
+      ArLog::log(ArLog::Terse, "Could not connect to robot, will not have parameter file so options displayed later may not include everything");
+    }
+    // otherwise abort
+    else
+    {
+      ArLog::log(ArLog::Terse, "Error, could not connect to robot.");
+      Aria::logOptions();
+      Aria::exit(1);
+    }
+  }
+
+  if(!robot.isConnected())
+  {
+    ArLog::log(ArLog::Terse, "Internal error: robot connector succeeded but ArRobot::isConnected() is false!");
+  }
+
+  // Start the robot task loop running in a new background thread. The 'true' argument means if it loses
+  // connection the task loop stops and the thread exits.
+  robot.runAsync(true);
+
+  // We need to lock the robot since we'll be setting up these modes
+  // while the robot task loop thread is already running, and they
+  // need to access some shared data in ArRobot.
+  robot.lock();
+
+  // turn on the motors
+  robot.comInt(ArCommands::ENABLE, 1);
+
+  robot.unlock();
+
+  Aria::exit(0); //Exit Aria
+  return 0;
+
 }
